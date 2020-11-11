254d7154a69fa80d195f9529017e819d84d12c36
Carter Borst <borst@ucar.edu>
2020-11-11 13:12:21 -0700

    Adding in headerline print, and windspeed to data dict

--- mysci.py
+++ mysci.py
@@ -1,8 +1,20 @@
-i# Column names and column indices to read
-columns = {'date': 0, 'time': 1, 'tempout': 2}
+# Column names and column indices to read
+columns = {'date': 0, 'time': 1, 'tempout': 2, 'windspeed':7,
+           'windchill': 12}
 
 # Data types for each column (only if non-string)
-types = {'tempout': float}
+types = {'tempout': float, 'windspeed': float, 'windchill': float}
+
+# Compute the wind chill temperature
+def compute_windchill(t, v):
+   a = 35.74
+   b = 0.6215
+   c = 35.75
+   d = 0.4275
+
+   v16 = v ** 0.16
+   wci = a + (b * t) - (c * v16) + (d * t * v16)
+   return wci
 
 # Initialize my data variable
 data = {}
@@ -15,7 +27,8 @@ with open(filename, 'r') as datafile:
 
    # Read the first three lines (header)
    for _ in range(3):
-      datafile.readline()
+      headerline = datafile.readline()
+      # print(headerline)
 
    # Read and parse the rest of the file
    for line in datafile:
@@ -25,3 +38,20 @@ with open(filename, 'r') as datafile:
          t = types.get(column, str)
          value = t(split_line[i])
          data[column].append(value)
+
+#windchill = []
+#for temp, windspeed in zip(data['tempout'], data['windspeed']):
+#    windchill.append(compute_windchill(temp, windspeed))
+
+# Output comparison of data
+# print('                ORIGINAL  COMPUTED')
+# print(' DATE    TIME  WINDCHILL WINDCHILL DIFFERENCE')
+# print('------- ------ --------- --------- ----------')
+# zip_data = zip(data['date'], data['time'], data['windchill'], windchill)
+# for date, time, wc_orig, wc_comp in zip_data:
+   wc_diff = wc_orig - wc_comp
+   print(f'{date} {time:>6} {wc_orig:9.6f} {wc_comp:9.6f} {wc_diff:10.6f}')
+
+# DEBUG
+# print(data['tempout'])
+# print(windchill)
